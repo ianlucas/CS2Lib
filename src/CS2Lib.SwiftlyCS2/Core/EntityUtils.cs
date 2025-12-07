55dbd79b85e3aa9c435257467eac99b8afab0cb1
Ian Lucas <ianlucas_ferreira@hotmail.com>
2025-12-07 15:32:33 -0300

    Use Swiftly type for teams

--- src/CS2Lib.SwiftlyCS2/Core/EntityUtils.cs
+++ src/CS2Lib.SwiftlyCS2/Core/EntityUtils.cs
@@ -3,20 +3,17 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+using SwiftlyS2.Shared.Players;
+
 namespace CS2Lib.SwiftlyCS2.Core;
 
 public static class EntityUtils
 {
-    public const byte None = 0;
-    public const byte Spectator = 1;
-    public const byte T = 2;
-    public const byte CT = 3;
-
-    public static byte ToggleTeam(byte team) =>
-        team > Spectator
-            ? team == T
-                ? CT
-                : T
+    public static Team ToggleTeam(Team team) =>
+        team > Team.Spectator
+            ? team == Team.T
+                ? Team.CT
+                : Team.T
             : team;
 
     public static string GetAgentModelPath(string model) => $"characters/models/{model}.vmdl";
